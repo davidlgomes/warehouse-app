705627007551abbad24bcc2c64f333593f92e0f9
davidlgomes <gomes.david.9@gmail.com>
2022-05-04 02:50:42 -0300

    Listando Galpões Finalizado

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
     def index
-    
+        @warehouse = Warehouse.all
     end
 end
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -9,4 +9,23 @@ describe 'Usuário Visita Tela Inicial' do
         #Assert
             expect(page).to have_content('Sistema de Galpões e Estoque')
     end
+    it 'e vê os galpões cadastrados' do
+        #Arrange
+            Warehouse.create(name:'Rio' , code:'SDU', city:'Rio de Janeiro', area:60_000)
+            Warehouse.create(name:'Maceio', code:'MCZ', city:'Maceio', area:50_000)
+        #Act
+            visit('/')
+        #Assert
+            expect(page).not_to have_content('Não existe galpões cadastrados')
+            expect(page).to have_content('Rio')
+            expect(page).to have_content('Codigo: SDU')
+            expect(page).to have_content('Cidade: Rio de Janeiro')
+            expect(page).to have_content('60000 m2')
+
+            expect(page).to have_content('Maceio')
+            expect(page).to have_content('Codigo: MCZ')
+            expect(page).to have_content('Cidade: Maceio')
+            expect(page).to have_content('50000 m2')
+    end
+    
 end
