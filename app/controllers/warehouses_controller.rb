3865483c97365332c52cb9c3f89acae12bf5d6fc
davidlgomes <gomes.david.9@gmail.com>
2022-05-06 02:21:40 -0300

    Criando Cadastro

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,5 +1,20 @@
 class WarehousesController < ApplicationController
     def show
-        @warehouse=Warehouse.find(params[:id])
+        id = params[:id]
+        if !Warehouse.all.empty?
+            @warehouse=Warehouse.find(id)
+        end
+        return 'Não foi cadastrado'
+    end
+    def new
+    
+    end
+
+    def create
+        warehouse_params=params.require(:warehouse).permit(:name, :code, 
+                                        :city, :description, :cep, :area) 
+        warehouse = Warehouse.new(warehouse_params)
+        warehouse.save
+        redirect_to root_path
     end
 end
